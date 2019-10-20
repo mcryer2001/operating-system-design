2a24802732be0aa5ef7c8517ac984f9f6127a717
Luke Evans <luevans248@gmail.com>
2019-10-20 10:31:29 -0700

    add code to complete lab2 task

--- lab2/Lab_2_Team_2.c
+++ lab2/Lab_2_Team_2.c
@@ -1,3 +1,49 @@
-/* Place code in this file */ 
+/* Luke Evans, David Howick, Kenneth Smith, William Scott, Michael Cryer */
+/* Lab_2_Team_2.c - main, signaler, waiter */
 
-/* changed by Luke */
+#include <Xinu.h>
+
+void signaler(), waiter();
+int  sem_start_value = 20, count_start = 0, count_stop = 2000;
+char * waiter_sentence = "Wow, I am running \n";
+sid32 sem;
+
+/*------------------------------------------------------------------------
+ *  main  -  Creates the signaler and waiter processes
+ *------------------------------------------------------------------------
+ */
+int main(void)
+{
+  sem = semcreate(sem_start_value);
+  pid32 signaler_pid = create(signaler, 1024, 20, "signaler", 0);
+  pid32 waiter_pid   = create(waiter, 1024, 40, "waiter", 1, signaler_pid);
+  resume(signaler_pid);
+  resume(waiter_pid);
+}
+
+/*------------------------------------------------------------------------
+*  waiter -  Prints numbers from count_start to count_stop
+*------------------------------------------------------------------------
+*/
+void waiter(pid32 signaler_pid)
+{
+  int i;
+  for(i = count_start; i <= count_stop; i++) {
+    kprintf("%d\n", i);
+    wait(sem);
+  }
+  kill(signaler_pid);
+}
+
+/*------------------------------------------------------------------------
+*  signaler  -  Prints a sentence
+*------------------------------------------------------------------------
+*/
+void signaler(void)
+{
+  yield();
+  while(1) {
+    kprintf("%s", waiter_sentence);
+    signaln(sem, 5);
+  }
+}
