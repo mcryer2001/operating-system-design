90cb0f9b8f1bfbe38b157e2f91744bb2e00ab238
mcryer2001 <32718473+mcryer2001@users.noreply.github.com>
2019-12-03 23:19:43 -0500

    Update main.c

--- lab6/system/main.c
+++ lab6/system/main.c
@@ -1,33 +1,34 @@
-/* main.c - main */ 
-#include <xinu.h> 
-int ROUND_ROBIN = 0; 
-void receivemessageprocess(); 
-pid32 pid; 
-void main(void) 
-	{ 
-		// ready(create((void *)shell, INITSTK, INITPRIO, "SHELL0", 3, 
-		// CONSOLE, CONSOLE, CONSOLE), 
-		// RESCHED_NO); 
-		pid = create(receivemessageprocess, 1024, 21, "Process 2", 0); 
-		resume(pid); 
-		sendk(pid, "this \n"); 
-		sendk(pid, "does \n"); 
-		sendk(pid, "multiple \n"); 
-		sendk(pid, "messages \n"); 
-		sendk(pid, "last message possible \n"); 
-		sendk(pid, "message 6 \n"); 
-		sendk(pid, "message 7 \n"); 
-		sendk(pid, "message 8 \n"); 
-		sendk(pid, "message 9 \n"); 
-		sendk(pid, "message 10 \n"); 
-		// char test[] = "This works at least"; 
-		// kprintf("%s\n", test); 
-		} 
-		void receivemessageprocess() 
-		{ 
-			sleep(5); int i = 0; 
-			for (i; i < NMSG; i++) { 
-				umsg32 str = receivek(); 
-				kprintf("%s", str); 
-			} 
-		}
+/*main.c - main */ 
+#include <xinu.h>
+
+void receive_msg();
+pid32 pid;
+
+void main(void)
+{
+	pid = create(receive_msg, 1024, 21, "Process 2", 0);
+
+	resume(pid);
+
+	sendk(pid, "this");
+	sendk(pid, "does");
+	sendk(pid, "multiple");
+	sendk(pid, "messages");
+	sendk(pid, "last message possible");
+	sendk(pid, "message 6");
+	sendk(pid, "message 7");
+	sendk(pid, "message 8");
+	sendk(pid, "message 9");
+	sendk(pid, "message 10");
+}
+
+void receive_msg()
+{
+	sleep(5);
+	int i = 0;
+	for (i; i < NMSG; i++)
+	{
+		umsg32 str = receivek();
+		kprintf("%s\n", str);
+	}
+}
