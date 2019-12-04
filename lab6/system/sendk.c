7efcd9908e7b1ef39fee6aabf4b4d873187930fd
mcryer2001 <32718473+mcryer2001@users.noreply.github.com>
2019-12-03 23:21:45 -0500

    Update sendk.c

--- lab6/system/sendk.c
+++ lab6/system/sendk.c
@@ -1,21 +1,21 @@
-/* sendk.c - sendk */
-
+/*sendk.c - sendk */
 #include <xinu.h>
 
 /*------------------------------------------------------------------------
- *  sendk  -  pass a message to a process and start recipient if waiting
+ *sendk  -  pass a message to a process and start recipient if waiting
  *------------------------------------------------------------------------
  */
-syscall	sendk(
-	  pid32		pid,		/* ID of recipient process	*/
-	  umsg32	msg		/* contents of message		*/
-	)
+syscall sendk(	pid32 pid,      /* ID of recipient process	*/
+	        umsg32 msg      /* contents of message		*/
+)
+
 {
-	intmask	mask;			/* saved interrupt mask		*/
-	struct	procent *prptr;		/* ptr to process' table entry	*/
+	intmask mask;           /*saved interrupt mask		*/
+	struct procent * prptr; /*ptr to process' table entry	*/
 
 	mask = disable();
-	if (isbadpid(pid)) {
+	if (isbadpid(pid))
+	{
 		restore(mask);
 		return SYSERR;
 	}
@@ -24,31 +24,37 @@ syscall	sendk(
 
 	prptr->msgcounter++;
 
-	//if ((prptr->prstate == PR_FREE) || prptr->prhasmsg) {
-	if (prptr->prstate == PR_FREE) {
+	if (prptr->prstate == PR_FREE)
+	{
 		restore(mask);
 		return SYSERR;
 	}
 
-  if (prptr->msgcounter > NMSG) {
-  	prptr->msgcounter--;
-    kprintf("%s! Original Message: %s\n", "Error processing message", msg);
-  } else {
-  	kprintf("Processing message: %s\n", msg);
-	 	prptr->messages[prptr->nextMsgIndex] = msg;
-	 	prptr->nextMsgIndex++;
-		prptr->prmsg    = msg;		/* deliver message		*/
-		prptr->prhasmsg = TRUE;		/* indicate message is waiting	*/
+	if (prptr->msgcounter > NMSG)
+	{
+		prptr->msgcounter--;
+		kprintf("%s! Original Message: %s\n", "Error processing message", msg);
+	}
+	else
+	{
+		kprintf("Processing message: %s\n", msg);
+		prptr->messages[prptr->nextMsgIndex] = msg;
+		prptr->nextMsgIndex++;
+		prptr->prmsg = msg;     /*deliver message		*/
+		prptr->prhasmsg = TRUE; /*indicate message is waiting	*/
 	}
 
-	/* If recipient waiting or in timed-wait make it ready */
+	/*If recipient waiting or in timed-wait make it ready */
 
-	if (prptr->prstate == PR_RECV) {
+	if (prptr->prstate == PR_RECV)
+	{
 		ready(pid, RESCHED_YES);
-	} else if (prptr->prstate == PR_RECTIM) {
+	}
+	else if (prptr->prstate == PR_RECTIM)
+	{
 		unsleep(pid);
 		ready(pid, RESCHED_YES);
 	}
-	restore(mask);		/* restore interrupts */
+	restore(mask); /*restore interrupts */
 	return OK;
 }
